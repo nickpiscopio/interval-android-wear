c504d41abf58058337d8c5c85fb8d1cbf1064c6d
nickpiscopio <nick.piscopio@gmail.com>
2016-05-28 13:19:43 -0400

    Added the start screen for the mobile version.

--- mobile/src/main/java/com/intencity/interval/view/MainActivity.java
+++ mobile/src/main/java/com/intencity/interval/view/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ScrollView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.intencity.interval.R;
@@ -13,14 +13,13 @@ import com.intencity.interval.functionality.view.Main;
 
 public class MainActivity extends AppCompatActivity
 {
-    private final int INTERVALS_ID = R.id.text_view_interval;
-    private final int INTERVAL_TIME_ID = R.id.text_view_interval_time;
-    private final int INTERVAL_REST_ID = R.id.text_view_interval_rest;
-
     private Context context;
 
-    private ScrollView containerView;
+    private LinearLayout intervalLayout;
+    private LinearLayout intervalTimeLayout;
+    private LinearLayout intervalRestLayout;
     private TextView titleTextView;
+    private TextView selectedTextView;
     private TextView intervalTextView;
     private TextView intervalTimeTextView;
     private TextView intervalRestTextView;
@@ -37,8 +36,12 @@ public class MainActivity extends AppCompatActivity
 
         context = getApplicationContext();
 
-        containerView = (ScrollView)findViewById(R.id.container);
+        intervalLayout = (LinearLayout) findViewById(R.id.layout_intervals);
+        intervalTimeLayout = (LinearLayout) findViewById(R.id.layout_interval_time);
+        intervalRestLayout = (LinearLayout) findViewById(R.id.layout_interval_rest);
+
         titleTextView = (TextView)findViewById(R.id.title);
+        selectedTextView = (TextView)findViewById(R.id.text_view_editing);
         intervalTextView = (TextView)findViewById(R.id.text_view_interval);
         intervalTimeTextView = (TextView)findViewById(R.id.text_view_interval_time);
         intervalRestTextView = (TextView)findViewById(R.id.text_view_interval_rest);
@@ -46,6 +49,6 @@ public class MainActivity extends AppCompatActivity
         decrementInterval = (ImageButton)findViewById(R.id.button_decrement);
         start = (Button) findViewById(R.id.start);
 
-        new Main(context, INTERVALS_ID, INTERVAL_TIME_ID, INTERVAL_REST_ID, titleTextView, intervalTextView, intervalTimeTextView, intervalRestTextView, incrementInterval, decrementInterval, start, IntervalActivity.class);
+        new Main(context, intervalLayout, intervalTimeLayout, intervalRestLayout, titleTextView, selectedTextView, intervalTextView, intervalTimeTextView, intervalRestTextView, incrementInterval, decrementInterval, start, IntervalActivity.class);
     }
 }
